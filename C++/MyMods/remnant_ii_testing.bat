0461ab9063582844a2a92d0260a6da81d6ab64ff
Mythical-Github <MythicalData@gmail.com>
2023-07-28 18:20:00 -0400

    More Automation Script Stuff
    More Automation Script Stuff

--- C++/MyMods/remnant_ii_testing.bat
+++ C++/MyMods/remnant_ii_testing.bat
@@ -2,15 +2,19 @@
 setlocal
 
 
-set "old_dll=C:\games\manual_install\Remnant2\Remnant2\Binaries\Win64\Mods\ExampleMod\dlls\main.dll"
-set "new_dll=C:\Users\Mythical\Documents\GitHub\Remnant2ExampleMods\C++\MyMods\Output\ExampleMod\Debug\ExampleMod.dll"
+set "old_ue4ss_cpp_dll="
+set "new_ue4ss_cpp_dll="
+set "old_ue4ss_xinput_dll="
+set "new_ue4ss_xinput_dll="
+set "old_example_mod_dll=C:\games\manual_install\Remnant2\Remnant2\Binaries\Win64\Mods\ExampleMod\dlls\main.dll"
+set "new_example_mod_dll=C:\Users\Mythical\Documents\GitHub\Remnant2ExampleMods\C++\MyMods\Output\ExampleMod\Debug\ExampleMod.dll"
 set "game_exe=C:\games\manual_install\Remnant2\Remnant2\Binaries\Win64\Remnant2-Win64-Shipping.exe"
 
 
 cd "%~dp0%"
 
 
-for %%A in ("%new_dll%" "%old_dll%") do (
+for %%A in ("%new_ue4ss_cpp_dll%" "%old_ue4ss_cpp_dll%") do (
     set "folder=%%~dpA"
     if not exist "!folder!" (
         echo Creating directory: "!folder!"
@@ -19,13 +23,46 @@ for %%A in ("%new_dll%" "%old_dll%") do (
 )
 
 
-if exist "%new_dll%" (
-    if exist "%old_dll%" (
-        del "%old_dll%"
+for %%A in ("%new_ue4ss_xinput_dll%" "%old_ue4ss_xinput_dll%") do (
+    set "folder=%%~dpA"
+    if not exist "!folder!" (
+        echo Creating directory: "!folder!"
+        mkdir "!folder!"
     )
-    copy "%new_dll%" "%old_dll%"
+)
+
+
+for %%A in ("%new_example_mod_dll%" "%old_example_mod_dll%") do (
+    set "folder=%%~dpA"
+    if not exist "!folder!" (
+        echo Creating directory: "!folder!"
+        mkdir "!folder!"
+    )
+)
+
+
+if exist "%new_ue4ss_cpp_dll%" (
+    if exist "%old_ue4ss_cpp_dll%" (
+        del "%old_ue4ss_cpp_dll%"
+    )
+    copy "%new_ue4ss_cpp_dll%" "%old_ue4ss_cpp_dll%"
+)
+
+
+if exist "%new_ue4ss_xinput_dll%" (
+    if exist "%old_ue4ss_xinput_dll%" (
+        del "%old_ue4ss_xinput_dll%"
+    )
+    copy "%new_ue4ss_xinput_dll%" "%old_ue4ss_xinput_dll%"
+)
+
+
+if exist "%new_example_mod_dll%" (
+    if exist "%old_example_mod_dll%" (
+        del "%old_example_mod_dll%"
+    )
+    copy "%new_example_mod_dll%" "%old_example_mod_dll%"
 )
 
 
 start "" "%game_exe%"
-
