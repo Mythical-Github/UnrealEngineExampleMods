606dd267289ecf47f7a5ae00a9242bb1bc9aa775
Mythical-Github <MythicalData@gmail.com>
2023-07-28 20:00:33 -0400

    stuff
    stuff

--- C++/MyDllMods/full_build_setup_and_testing.bat
+++ C++/MyDllMods/full_build_setup_and_testing.bat
@@ -6,19 +6,19 @@ rem These are the ones you need to manually specify
 
 
 rem This is the main game folder, like "../steamapps/common/Remnant2"
-rem Example
+rem Example:
 rem set "game_dir=C:\games\manual_install\Remnant2"
 set "game_dir=C:\games\manual_install\Remnant2"
 
 
 rem This is the game project name, like the latter dir here "../steamapps/common/Remnant2/Remnant2"
-rem Example
+rem Example:
 rem set "game_project_name=Remnant2"
 set "game_project_name=Remnant2"
 
 
 rem This is the name of the game's exe in the Binaries/Win64 directory
-rem Example
+rem Example:
 rem set "%game_project_name%=Remnant2-Win64-Shipping.exe"
 set "%game_project_name%=Remnant2-Win64-Shipping.exe"
 
